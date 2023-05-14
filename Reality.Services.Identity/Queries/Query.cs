09587279e7b59c1fcd2907717ffde28fcdb19f0e
Gabriel Spalato <3950465+gspalato@users.noreply.github.com>
2023-05-14 13:24:59 -0300

    Inserted a 401 code error when authentication fails.

--- Reality.Services.Identity/Queries/Query.cs
+++ Reality.Services.Identity/Queries/Query.cs
@@ -9,8 +9,19 @@ namespace Reality.Services.Identity.Queries
     public class Query
     {
         public async Task<AuthorizationPayload> IsAuthenticatedAsync(string token, [Service] IAuthorizationService authService) {
+            var invalidTokenError = ErrorBuilder.New()
+                .SetMessage("Invalid token. Try regenerating your token.")
+                .SetCode("401")
+                .Build();
+            
+            if (token.Length < 0 || token is null)
+                throw new GraphQLException(invalidTokenError);
+
             var result = await authService.CheckAuthorizationAsync(token);
             var roles = authService.ExtractRoles(result);
+
+            if (!result.IsValid)
+                throw new GraphQLException(invalidTokenError);
 
             return new() {
                 Successful = result.IsValid,
