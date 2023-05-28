a12168bafae473869276682c5921a698206b8cf8
Gabriel Spalato <3950465+gspalato@users.noreply.github.com>
2023-05-28 15:31:25 -0300

    Trying to fix subscriptions.

--- Reality.Services.IoT.UPx/Types/Subscription.cs
+++ Reality.Services.IoT.UPx/Types/Subscription.cs
@@ -8,7 +8,7 @@ namespace Reality.Services.IoT.UPx.Types
 {
     public class Subscription
     {
-        [Subscribe]
+        [Subscribe(MessageType = typeof(IEnumerable<Resume>))]
         [Topic("OnStationUpdate")]
         public async Task<IEnumerable<Resume>> OnStationUpdate([Service] IUseRepository useRepository)
             => await new Query().GetResumesAsync(useRepository);
