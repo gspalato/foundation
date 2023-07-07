19704fb6c42a39ca6ec848b89c255314f1698b02
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-07 01:33:05 -0300

    Simplified logging.

--- Reality.Common/Configurations/LoggingConfiguration.cs
+++ Reality.Common/Configurations/LoggingConfiguration.cs
@@ -8,18 +8,7 @@ namespace Reality.Common.Configurations
         public LoggingConfiguration()
         {
             var console = new NLog.Targets.ColoredConsoleTarget("console");
-            console.Layout = @"[${date:format=dd-MM-yyyy HH\:mm\:ss}] ${level:format=Name}: ${message} ${onexception:inner=\n${exception:format=tostring}}}";
-
-            var rowRules = console.RowHighlightingRules;
-            var wordRules = console.WordHighlightingRules;
-            var unchanged = ConsoleOutputColor.NoChange;
-
-            wordRules.Add(new ConsoleWordHighlightingRule("Trace:", ConsoleOutputColor.Green, unchanged));
-            wordRules.Add(new ConsoleWordHighlightingRule("Debug:", ConsoleOutputColor.Green, unchanged));
-            wordRules.Add(new ConsoleWordHighlightingRule("Info:", ConsoleOutputColor.Blue, unchanged));
-            wordRules.Add(new ConsoleWordHighlightingRule("Warn:", ConsoleOutputColor.Yellow, unchanged));
-            wordRules.Add(new ConsoleWordHighlightingRule("Error:", ConsoleOutputColor.DarkRed, unchanged));
-            wordRules.Add(new ConsoleWordHighlightingRule("Fatal:", ConsoleOutputColor.Red, unchanged));
+            console.Layout = @"[${date:format=dd-MM-yyyy HH\:mm\:ss}] ${level:format=Name}: ${message} ${onexception:inner=\n${exception:format=tostring}}";
 
             this.AddRule(LogLevel.Trace, LogLevel.Fatal, console);
         }
