7e500ea5a5fff12f275f1f107894f791a3c6859f
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-08 22:57:23 -0300

    Updated Project entity

--- Reality.Common/Entities/Project.cs
+++ Reality.Common/Entities/Project.cs
@@ -3,8 +3,9 @@
     public class Project : BaseEntity
     {
         public string Name { get; set; } = default!;
-        public string IconUrl { get; set; } = default!;
-        public string Description { get; set; } = default!;
-        public string Url { get; set; } = default!;
+        public string? IconUrl { get; set; } = default!;
+        public string? Description { get; set; } = default!;
+        public string? RepositoryUrl { get; set; } = default!;
+        public string? DeploymentUrl { get; set; } = default!;
     }
 }
