5863cddbfbe8d743a59538f126ba95e153708f01
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-10 20:49:08 -0300

    Added project banners and image fallbacks.

--- Reality.Common/Entities/Project.cs
+++ Reality.Common/Entities/Project.cs
@@ -3,9 +3,41 @@
     public class Project : BaseEntity
     {
         public string Name { get; set; } = default!;
+
+        /// <summary>
+        /// The URL to the icon image for the project.
+        /// </summary>
+        /// <remarks>
+        /// The file is a <b>.webp</b> file.
+        /// </remarks>
         public string? IconUrl { get; set; } = default!;
+        /// <summary>
+        /// The Fallback URL to the icon image for the project.
+        /// </summary>
+        /// <remarks>
+        /// The file is a <b>.webp</b> file.
+        /// </remarks>
+        public string? FallbackIconUrl { get; set; } = default!;
+
+        /// <summary>
+        /// The URL to the banner image for the project.
+        /// </summary>
+        /// <remarks>
+        /// The file is a <b>.webp</b> file.
+        /// </remarks>
+        public string? BannerUrl { get; set; } = default!;
+        /// <summary>
+        /// The Fallback URL to the banner image for the project.
+        /// </summary>
+        /// <remarks>
+        /// The file is a <b>.jpg</b> filee
+        /// </remarks>
+        public string? FallbackBannerUrl { get; set; } = default!;
+
         public string? Description { get; set; } = default!;
+
         public string? RepositoryUrl { get; set; } = default!;
+
         public string? DeploymentUrl { get; set; } = default!;
     }
 }
