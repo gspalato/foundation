8e33564017b98e3aa1320159c4fd631fc9ba3687
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-11 20:15:48 -0300

    Added an experimental kubernetes tool.

---
+++ Reality.Kubernetes/tool.py
@@ -0,0 +1,69 @@
+import os
+import typer
+import subprocess
+
+# Configuration files
+registry_host = 'localhost:5000'
+secrets_filename = 'secrets.yml'
+service_folder_order = [
+    'Reality.Database',
+    'Reality.Services.Identity',
+    'Reality.Services.UPx',
+    'Reality.Services.Portfolio',
+    'Portfolio',
+    'Reality.Gateway',
+    'Reality.Proxy',
+]
+
+
+# App
+app = typer.Typer()
+
+@app.command("build")
+def build_images():
+    print("Building Docker images...")
+    subprocess.run(["docker", "login", registry_host])
+
+    # Build images
+    for folder in service_folder_order:
+        print("Building " + folder + "...")
+        dockerfile = os.path.join('./', folder, 'Dockerfile')
+        tag = os.path.join(registry_host, folder)
+        subprocess.run(["docker", "build", "-t", tag, "-f", dockerfile, "."])
+
+        print("Pushing " + folder + " to local registry...")
+        subprocess.run(["docker", "push", tag])
+
+@app.command("up")
+def start_kubernetes():
+    print("Starting Reality...")
+    
+    # Apply secrets
+    secrets = os.path.join('../', secrets_filename)
+    subprocess.run(["kubectl", "apply", "-f", secrets])
+
+    # Apply service configurations
+    for folder in service_folder_order:
+        service = os.path.join('./', folder, "kubernetes.yml")
+        subprocess.run(["kubectl", "apply", "-f", service])
+
+@app.command("update")
+def update_kubernetes():
+    print("Updating Kubernetes configuration...")
+
+    # Apply service configurations
+    for folder in service_folder_order:
+        service = os.path.join('./', folder, "kubernetes.yml")
+        subprocess.run(["kubectl", "apply", "-f", service])
+
+@app.command("down")
+def stop_kubernetes():
+    print("Stopping Reality...")
+
+    subprocess.run(["kubectl", "delete", "-all"])
+    
+
+
+
+if __name__ == "tool":
+    app()
