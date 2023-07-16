563f1161dcb3ea55126e37dd8fed45501725ae5b
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-16 14:46:49 -0300

    Fixed directories.

--- tools/rctl/lib/directories.py
+++ tools/rctl/lib/directories.py
@@ -1,6 +1,7 @@
 from os import path
 
 FILE_DIR = path.dirname(path.abspath(__file__))
-TOOLS_DIR = path.dirname(FILE_DIR)
+RCTL_DIR = path.dirname(FILE_DIR)
+TOOLS_DIR = path.dirname(RCTL_DIR)
 ROOT_DIR = path.dirname(TOOLS_DIR)
 SRC_DIR = path.join(ROOT_DIR, "src")
