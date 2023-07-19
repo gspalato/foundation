bae2f6e72a43ba1f306fa77cf828681de64859ab
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-19 01:38:17 -0300

    Added SDK to Dockerfile.

--- src/Reality.Services.Gateway/Program.cs
+++ src/Reality.Services.Gateway/Program.cs
@@ -5,55 +5,46 @@ using Reality.SDK.API.GraphQL;
 using Reality.SDK.Database.Mongo;
 using Reality.Services.Gateway.Configurations;
 
-namespace Reality.Services.Gateway
-{
-    public static class Program
+new ServiceBuilder(args)
+    .LoadConfiguration<GatewayConfiguration>()
+    .UseMongo()
+    .UseGraphQL("/gql", (server, services) =>
     {
-        public static void Main(string[] args)
+        var provider = services.BuildServiceProvider();
+        var logger = provider.GetRequiredService<ILogger<ServiceBuilder>>();
+        var config = provider.GetRequiredService<GatewayConfiguration>();
+
+        // GraphQL HTTP Schema Stitching
+        foreach (var url in config.Service_Urls.Split(","))
         {
-            new ServiceBuilder(args)
-                .LoadConfiguration<GatewayConfiguration>()
-                .UseMongo()
-                .UseGraphQL("/gql", (server, services) =>
-                {
-                    var provider = services.BuildServiceProvider();
-                    var logger = provider.GetRequiredService<ILogger<ServiceBuilder>>();
-                    var config = provider.GetRequiredService<GatewayConfiguration>();
+            var id = url.Replace("http://", "").Replace("-", "_");
+            services.AddHttpClient(id, (_, client) =>
+            {
+                client.BaseAddress = new Uri(new Uri(url), "/gql");
+            });
+            services.AddWebSocketClient(id, (_, client) =>
+            {
+                client.Uri = new Uri(new Uri(url), "/gql");
+            });
 
-                    // GraphQL HTTP Schema Stitching
-                    foreach (var url in config.Service_Urls.Split(","))
-                    {
-                        var id = url.Replace("http://", "").Replace("-", "_");
-                        services.AddHttpClient(id, (_, client) =>
-                        {
-                            client.BaseAddress = new Uri(new Uri(url), "/gql");
-                        });
-                        services.AddWebSocketClient(id, (_, client) =>
-                        {
-                            client.Uri = new Uri(new Uri(url), "/gql");
-                        });
+            server.AddRemoteSchema(id, capabilities: new EndpointCapabilities
+            {
+                Batching = BatchingSupport.RequestBatching,
+                Subscriptions = SubscriptionSupport.WebSocket
+            });
 
-                        server.AddRemoteSchema(id, capabilities: new EndpointCapabilities
-                        {
-                            Batching = BatchingSupport.RequestBatching,
-                            Subscriptions = SubscriptionSupport.WebSocket
-                        });
-
-                        logger.LogDebug($"Added remote schema \"{id}\"");
-                    }
-                })
-                .Configure((WebApplication app) =>
-                {
-                    app.UseCors(builder => builder
-                        .AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-                })
-                .Build()
-                .Run();
+            logger.LogDebug($"Added remote schema \"{id}\"");
         }
-    }
-}
+    })
+    .Configure((WebApplication app) =>
+    {
+        app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+    })
+    .Build()
+    .Run();
 /*
 namespace Reality.Services.Gateway
 {
