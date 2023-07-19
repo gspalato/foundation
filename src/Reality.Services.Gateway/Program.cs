6521a591817f35859b48e89688b272af81835438
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-19 01:26:36 -0300

    Testing the Gateway service using Reality.SDK.

--- src/Reality.Services.Gateway/Program.cs
+++ src/Reality.Services.Gateway/Program.cs
@@ -1,5 +1,51 @@
+using HotChocolate.Stitching;
 using Microsoft.AspNetCore;
+using Reality.SDK;
+using Reality.SDK.API.GraphQL;
+using Reality.SDK.Database.Mongo;
+using Reality.Services.Gateway.Configurations;
 
+new ServiceBuilder(args)
+    .LoadConfiguration<GatewayConfiguration>()
+    .UseMongo()
+    .UseGraphQL("/gql", (server, services) =>
+    {
+        var provider = services.BuildServiceProvider();
+        var logger = provider.GetRequiredService<ILogger<ServiceBuilder>>();
+        var config = provider.GetRequiredService<GatewayConfiguration>();
+
+        // GraphQL HTTP Schema Stitching
+        foreach (var url in config.Service_Urls.Split(","))
+        {
+            var id = url.Replace("http://", "").Replace("-", "_");
+            services.AddHttpClient(id, (_, client) =>
+            {
+                client.BaseAddress = new Uri(new Uri(url), "/gql");
+            });
+            services.AddWebSocketClient(id, (_, client) =>
+            {
+                client.Uri = new Uri(new Uri(url), "/gql");
+            });
+
+            server.AddRemoteSchema(id, capabilities: new EndpointCapabilities
+            {
+                Batching = BatchingSupport.RequestBatching,
+                Subscriptions = SubscriptionSupport.WebSocket
+            });
+
+            logger.LogDebug($"Added remote schema \"{id}\"");
+        }
+    })
+    .Configure((WebApplication app) =>
+    {
+        app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+    })
+    .Build()
+    .Run();
+/*
 namespace Reality.Services.Gateway
 {
     public static class Program
@@ -17,3 +63,4 @@ namespace Reality.Services.Gateway
         }
     }
 }
+*/
