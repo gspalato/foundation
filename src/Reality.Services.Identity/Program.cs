4a8c88267df4d237bc79f3c64a1b3205e8e38736
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-19 02:37:21 -0300

    Migrated Identity Service to the Reality.SDK pattern.

--- src/Reality.Services.Identity/Program.cs
+++ src/Reality.Services.Identity/Program.cs
@@ -1,5 +1,53 @@
-using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Reality.Common.Configurations;
+using Reality.Common.Services;
+using Reality.SDK;
+using Reality.SDK.API.GraphQL;
+using Reality.SDK.Database.Mongo;
+using Reality.Services.Identity.Services;
+using Reality.Services.Identity.Types;
+using System.IdentityModel.Tokens.Jwt;
 
+new ServiceBuilder(args)
+    .LoadConfiguration<BaseConfiguration>()
+    .UseMongo()
+    .UseGraphQL("/gql", (server, services, builder) =>
+    {
+        server.AddQueryType<Query>();
+        server.AddMutationType<Mutation>();
+
+        server
+            .AddMongoDbFiltering()
+            .AddMongoDbPagingProviders()
+            .AddMongoDbProjections()
+            .AddMongoDbSorting()
+            .AddDefaultTransactionScopeHandler();
+    })
+    .Configure((WebApplicationBuilder appBuilder) =>
+    {
+        appBuilder.Services
+            .AddSingleton<IAuthenticationService, AuthenticationService>()
+            .AddSingleton<IAuthorizationService, AuthorizationService>()
+            .AddSingleton<IUserService, UserService>()
+            .AddSingleton<IPasswordHasher<string>, PasswordHasher<string>>();
+
+        appBuilder.Services
+            .AddSingleton<JwtSecurityTokenHandler>()
+            .AddAuthorization()
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = Reality.Common.Configurations.TokenConfiguration.ValidationParameters);
+
+    })
+    .Configure((WebApplication app) =>
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+    })
+    .Build()
+    .Run();
+
+/*
 namespace Reality.Services.Identity
 {
     public static class Program
@@ -17,3 +65,4 @@ namespace Reality.Services.Identity
         }
     }
 }
+*/
