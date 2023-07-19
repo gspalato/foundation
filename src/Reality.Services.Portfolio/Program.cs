f50a345ce34f3da600ff06449532dbf090a75ea2
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-19 14:21:59 -0300

    Migrated Portfolio Service to Reality.SDK.

--- src/Reality.Services.Portfolio/Program.cs
+++ src/Reality.Services.Portfolio/Program.cs
@@ -1,5 +1,44 @@
-﻿using Microsoft.AspNetCore;
+﻿using Octokit;
+using Octokit.Internal;
+using Reality.SDK;
+using Reality.SDK.API.GraphQL;
+using Reality.SDK.Database.Mongo;
+using Reality.Services.Portfolio.Configurations;
+using Reality.Services.Portfolio.Services;
+using Reality.Services.Portfolio.Types;
+new ServiceBuilder(args)
+    .LoadConfiguration<Configuration>()
+    .UseMongo()
+    .UseGraphQL("/gql", (server, services, builder) =>
+    {
+        server.AddQueryType<Query>();
+        server.AddType<Project>();
 
+        server
+            .AddMongoDbFiltering()
+            .AddMongoDbPagingProviders()
+            .AddMongoDbProjections()
+            .AddMongoDbSorting();
+    })
+    .Configure((WebApplicationBuilder builder) =>
+    {
+        var githubToken = builder.Configuration.GetValue<string>("GithubToken");
+        if (string.IsNullOrEmpty(githubToken))
+            throw new Exception("Github token is not set!");
+
+        // Services
+        var productInfo = new Octokit.ProductHeaderValue("RealityAPI", "1.0.0");
+        var credentials = new Credentials(githubToken, AuthenticationType.Bearer);
+        var store = new InMemoryCredentialStore(credentials);
+
+        builder.Services.AddSingleton<GitHubClient>(_ => new GitHubClient(productInfo, store));
+
+        builder.Services.AddHostedService<ProjectService>();
+    })
+    .Build()
+    .Run();
+
+/*
 namespace Reality.Services.Portfolio
 {
     public static class Program
@@ -17,3 +56,4 @@ namespace Reality.Services.Portfolio
         }
     }
 }
+*/
