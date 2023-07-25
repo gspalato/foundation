d17bc19f3d5b0fa43c3c36210fd2e11cbc70c771
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-25 03:17:40 -0300

    QueryTypeSourceGenerator is now working.

--- src/Foundation.Core.Codegen/API/GraphQL/QueryTypeSourceGenerator.cs
+++ src/Foundation.Core.Codegen/API/GraphQL/QueryTypeSourceGenerator.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -9,125 +11,200 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Foundation.Core.Codegen.API.GraphQL;
 
+/// <summary>
+/// Stitches together all classes with the [Query] attribute and generates a new class called QueryType in the same namespace.
+/// The methods/queries are modified to include a execution time and database call tracking.
+/// More functionality can be added by modifying this source generator.
+/// </summary>
 [Generator]
 public class QueryTypeSourceGenerator : ISourceGenerator
 {
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(() => new QueryTypeSyntaxReceiver());
+
+        if (!Debugger.IsAttached)
+            Debugger.Launch();
+    }
+
     public void Execute(GeneratorExecutionContext context)
     {
-        Console.WriteLine("Executing QueryTypeSourceGenerator");
+        Console.WriteLine("Executing QueryTypeSourceGenerator.");
 
-        // Find the query class.
-        var foundTrees = context.Compilation.SyntaxTrees.Where(tree =>
-            tree
-                .GetRoot()
-                .DescendantNodes()
-                .OfType<ClassDeclarationSyntax>()
-                .Any(p =>
-                    p
-                        .DescendantNodes()
-                        .OfType<AttributeSyntax>()
-                        .Any()
-                )
-        );
+        if (context.SyntaxReceiver is not QueryTypeSyntaxReceiver receiver)
+            return;
 
-        var foundClasses = foundTrees.SelectMany(tree =>
-            tree
-                .GetRoot()
-                .DescendantNodes()
-                .OfType<ClassDeclarationSyntax>()
-        ).SelectMany(@class =>
-            @class
-                .DescendantNodes()
-                .OfType<AttributeSyntax>()
-        )
-        .Where(attr =>
-            attr
-                .Name
-                .GetText()
-                .ToString() == "GenerateQuery"
-        ).Select(attr => attr.Parent).OfType<ClassDeclarationSyntax>();
+        // Get the compilation that the generator is being executed in.
+        var compilation = context.Compilation;
 
-        var foundQueryClass = foundClasses.FirstOrDefault();
-        Console.WriteLine("Found query class: " + foundQueryClass.Identifier);
+        var queryMethods = new List<MethodDeclarationSyntax>();
 
-        // Find namespace node so it can be used to generate the source code in the same namespace.
-        // If the class is not in a namespace, then use the first namespace in the compilation.
-        SyntaxNode foundNamespaceNode = foundQueryClass.Parent;
-        if (foundNamespaceNode.GetFirstToken().Text != "namespace")
-            foundNamespaceNode = foundQueryClass.SyntaxTree.GetRoot().DescendantNodes().OfType<NamespaceDeclarationSyntax>().FirstOrDefault();
+        var usingDirectives = new List<UsingDirectiveSyntax>();
 
-        // Get all the using statements.
-        var foundUsings = foundQueryClass.SyntaxTree.GetRoot().DescendantNodes().OfType<UsingDirectiveSyntax>();
-        var foundUsingsSource = foundUsings.Select(u => u.GetText()).ToList();
-
-        // Get all methods.
-        var foundMethods = foundQueryClass.ChildNodes().OfType<MethodDeclarationSyntax>();
-
-        // Create new class and copy the methods, adding the code to track the execution time and database calls.
-        // Note: since C# source code is immutable, these changes return new syntaxes.
-        var queryClass = SyntaxFactory.ClassDeclaration("QueryType")
-            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-            .AddMembers(foundMethods.ToArray());
-
-        foreach (var foundMethod in foundMethods)
+        foreach (var foundQueryClass in receiver.CandidateClasses)
         {
-            var method = SyntaxFactory.MethodDeclaration(foundMethod.ReturnType, foundMethod.Identifier)
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .AddParameterListParameters(foundMethod.ParameterList.Parameters.ToArray())
-                .AddBodyStatements(foundMethod.Body.Statements.ToArray());
+            // Get all the using statements.
+            var foundUsings = foundQueryClass.SyntaxTree.GetRoot().DescendantNodes().OfType<UsingDirectiveSyntax>();
+            usingDirectives.AddRange(foundUsings);
 
-            method = method.InsertNodesBefore(foundMethod.Body.Statements.First(), new[]
+            // Get all methods.
+            var foundMethods = foundQueryClass.ChildNodes().OfType<MethodDeclarationSyntax>();
+
+            // Create a new edited method with the same name and return type.
+            foreach (var foundMethod in foundMethods)
             {
+                var method = SyntaxFactory
+                    .MethodDeclaration(foundMethod.ReturnType, foundMethod.Identifier)
+                    .AddModifiers(foundMethod.Modifiers.ToArray())
+                    .AddParameterListParameters(foundMethod.ParameterList.Parameters.ToArray());
+
+                var initialStatements = new[]
+                {
                     SyntaxFactory.ParseStatement("float __foundationCodegen_time = 0;"),
                     SyntaxFactory.ParseStatement("int __foundationCodegen_dbCalls = 0;"),
                     SyntaxFactory.ParseStatement("var __foundationCodegen_sw = new System.Diagnostics.Stopwatch();"),
                     SyntaxFactory.ParseStatement("__foundationCodegen_sw.Start();")
-            });
+                };
 
-            var lastNode = foundMethod.Body.Statements.Last().GetFirstToken().Text is "return"
-                ? foundMethod.Body.Statements[foundMethod.Body.Statements.Count() - 2]
-                : foundMethod.Body.Statements.Last();
+                method = method.AddBodyStatements(initialStatements);
 
-            method = method.InsertNodesAfter(lastNode, new[]
-            {
+                // Add actual logic.
+                var lastNodeIndex = foundMethod.Body.Statements.Last() is ReturnStatementSyntax returnStatement
+                    ? foundMethod.Body.Statements.Count() - 1
+                    : foundMethod.Body.Statements.Count();
+
+                var statements = foundMethod.Body.Statements;
+
+                // Finish by appending stopwatch stop and debug output before return or end of block.
+                // Note: For some reason this has to be in inverted order. Oh well.
+                // TODO: Finish the concept by adding a service that receives the data.
+                var finishingStatements = new[]
+                {
+                    SyntaxFactory.ParseStatement("System.Diagnostics.Debug.WriteLine($\"{__foundationCodegen_time}ms, {__foundationCodegen_dbCalls} db calls\");"),
                     SyntaxFactory.ParseStatement("__foundationCodegen_sw.Stop();"),
                     SyntaxFactory.ParseStatement("__foundationCodegen_time = __foundationCodegen_sw.ElapsedMilliseconds;"),
-                    SyntaxFactory.ParseStatement("System.Diagnostics.Debug.WriteLine($\"{__foundationCodegen_time}ms, {__foundationCodegen_dbCalls} db calls\");")
-            });
+                };
 
-            queryClass = queryClass.AddMembers(method);
+                foreach (var statement in finishingStatements)
+                    statements = statements.Insert(lastNodeIndex, statement);
+
+                method = method.AddBodyStatements(statements.ToArray());
+                queryMethods.Add(method);
+            }
         }
 
-        // Generate the source code
-        var namespaceNode = foundNamespaceNode.InsertTokensAfter(foundNamespaceNode.GetLastToken(), new SyntaxToken[]
+        // Find namespace node so it can be used to generate the source code in the same namespace.
+        // If the class is not in a namespace, then use the first namespace in the compilation.
+        // These are added to a namespaces list, so that the most common namespace can be used.
+        // In theory this should be the same for all classes, but it's possible that it's not.
+        // If no namespace is found, then the class is declared in the global namespace.
+        string namespaceIdentifier = "";
+        if (receiver.CandidateNamespaces.Count != 0)
         {
-                SyntaxFactory.ParseToken("."),
-                SyntaxFactory.ParseToken("Codegen")
-        });
-        var namespaceSource = namespaceNode.GetText().ToString();
+            var commonNamespace = receiver.CandidateNamespaces
+                .Where(n => n is not null)
+                .GroupBy(n =>
+                {
+                    if (n is NamespaceDeclarationSyntax namespaceDeclarationSyntax)
+                        return namespaceDeclarationSyntax.Name.ToFullString();
 
-        var classSource = queryClass.GetText().ToString();
+                    if (n is BaseNamespaceDeclarationSyntax baseNamespaceDeclarationSyntax)
+                        return baseNamespaceDeclarationSyntax.Name.ToFullString();
 
+                    return "";
+                })
+                .OrderByDescending(g => g.Count())
+                .FirstOrDefault()   // Get the group with the most items.
+                .FirstOrDefault();  // Get the first item in the group, as they should all be the same.
+
+            if (commonNamespace is NamespaceDeclarationSyntax namespaceDeclarationSyntax)
+                namespaceIdentifier = namespaceDeclarationSyntax.Name.ToFullString();
+            else if (commonNamespace is BaseNamespaceDeclarationSyntax baseNamespaceDeclarationSyntax)
+                namespaceIdentifier = baseNamespaceDeclarationSyntax.Name.ToFullString();
+        }
+        else
+            System.Diagnostics.Debug.WriteLine("No namespace found any class. Using global namespace.");
+
+
+        // Generate source code.
+        // Note: This was the best working solution yet.
+        //       Directly using the SyntaxFactory to create the source code was not working.
+        //       It didn't add spaces between keywords properly.
         var sourceBuilder = new StringBuilder();
-        foreach (var usingSource in foundUsingsSource)
-        {
-            sourceBuilder.AppendLine(usingSource.ToString());
-        }
 
-        sourceBuilder.AppendLine(namespaceSource.ToString());
+        sourceBuilder.AppendLine("// This file was automatically generated by Foundation.");
+        sourceBuilder.AppendLine("// It is a compilation of all [Query] classes, related to GraphQL APIs.");
+        sourceBuilder.AppendLine("// All methods were modified to include a execution time and database call tracking.");
+        sourceBuilder.AppendLine("// [Do not modify this file directly, as it will be overwritten.]");
+        sourceBuilder.AppendLine("// [Do not check this file into source control.]");
 
-        sourceBuilder.AppendLine(classSource.ToString());
+        usingDirectives.ForEach(d => sourceBuilder.Append(d.ToFullString()));
+
+        sourceBuilder.AppendLine($"namespace {namespaceIdentifier};");
+
+        sourceBuilder.AppendLine("public class QueryType\n{");
+
+        foreach (var method in queryMethods)
+            sourceBuilder.AppendLine(method.ToFullString());
+
+        sourceBuilder.AppendLine("}");
 
         // Add the source code to the compilation
-        context.AddSource($"QueryType.g.cs", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
-
-        // Test.
-        context.AddSource($"Test.g.cs", SourceText.From(@"Hello world!", Encoding.UTF8));
+        var source = SourceText.From(FormatCode(sourceBuilder.ToString()), Encoding.UTF8);
+        context.AddSource($"QueryType.g.cs", source);
     }
 
-    public void Initialize(GeneratorInitializationContext context)
+    /// <summary>
+    /// Formats the code to be more human readable.
+    /// </summary>
+    /// <param name="code">The code to format.</param>
+    /// <param name="cancelToken">The cancellation token.</param>
+    /// <returns>The formatted code.</returns>
+    /// <remarks>
+    /// Even though is code is autogenerated and will only be ran by the compiler,
+    /// it's still nice to have it formatted properly. This is especially useful for debugging.
+    /// </remarks>
+    private static string FormatCode(string code, CancellationToken cancelToken = default)
     {
-        if (!Debugger.IsAttached)
-            Debugger.Launch();
+        return CSharpSyntaxTree.ParseText(code, cancellationToken: cancelToken)
+            .GetRoot(cancelToken)
+            .NormalizeWhitespace()
+            .SyntaxTree
+            .GetText(cancelToken)
+            .ToString();
+    }
+}
+
+public class QueryTypeSyntaxReceiver : ISyntaxReceiver
+{
+    public List<SyntaxNode> CandidateNamespaces { get; } = new();
+    public List<ClassDeclarationSyntax> CandidateClasses { get; } = new();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax
+            && IsQueryClass(classDeclarationSyntax))
+        {
+            CandidateClasses.Add(classDeclarationSyntax);
+        }
+
+        if (syntaxNode is NamespaceDeclarationSyntax namespaceDeclarationSyntax && ContainsQueryClass(syntaxNode))
+            CandidateNamespaces.Add(namespaceDeclarationSyntax);
+
+        if (syntaxNode is BaseNamespaceDeclarationSyntax baseNamespaceDeclarationSyntax && ContainsQueryClass(syntaxNode.Parent))
+            CandidateNamespaces.Add(baseNamespaceDeclarationSyntax);
+    }
+
+    private static bool IsQueryClass(ClassDeclarationSyntax classDeclarationSyntax)
+    {
+        return classDeclarationSyntax.AttributeLists.SelectMany(l => l.Attributes).Any(a => a.Name.ToString() is "Query");
+    }
+
+    private static bool ContainsQueryClass(SyntaxNode node)
+    {
+        return node
+            .DescendantNodes()
+            .OfType<ClassDeclarationSyntax>()
+            .Any(c => IsQueryClass(c));
     }
 }
