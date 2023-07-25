1c1536972e70903e85d2247ffc8922fcb8734b18
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-24 21:07:06 -0300

    Foundation.Common now uses bracketless namespaces.

--- src/Foundation.Common/Role.cs
+++ src/Foundation.Common/Role.cs
@@ -1,54 +1,53 @@
-﻿namespace Foundation.Common.Roles
+﻿namespace Foundation.Common.Roles;
+
+public enum Role
 {
-    public enum Role
+    [RoleInfo(0)]
+    Owner,
+
+    [RoleInfo(1)]
+    Microservice,
+
+    [RoleInfo(2)]
+    Project,
+
+    [RoleInfo(3)]
+    Trusted,
+
+    [RoleInfo(4)]
+    User
+}
+
+public class RoleInfoAttribute : System.Attribute
+{
+    public int Priority;
+
+    public RoleInfoAttribute(int priority)
     {
-        [RoleInfo(0)]
-        Owner,
-
-        [RoleInfo(1)]
-        Microservice,
-
-        [RoleInfo(2)]
-        Project,
-
-        [RoleInfo(3)]
-        Trusted,
-
-        [RoleInfo(4)]
-        User
+        Priority = priority;
+    }
+}
+public static class RoleEnumExtension
+{
+    public static string? GetName(this Role role)
+    {
+        return Enum.GetName(role);
     }
 
-    public class RoleInfoAttribute : System.Attribute
+    public static string? GetName(this int n)
     {
-        public int Priority;
+        if (Enum.IsDefined(typeof(Role), n))
+            return Enum.GetName((Role)n);
 
-        public RoleInfoAttribute(int priority)
-        {
-            Priority = priority;
-        }
+        return null;
     }
-    public static class RoleEnumExtension
+
+    public static int? GetPriority(this Role role)
     {
-        public static string? GetName(this Role role)
-        {
-            return Enum.GetName(role);
-        }
+        RoleInfoAttribute[] attributes = (RoleInfoAttribute[])
+        role.GetType().GetField(Enum.GetName(role)!)!
+        .GetCustomAttributes(typeof(RoleInfoAttribute), false);
 
-        public static string? GetName(this int n)
-        {
-            if (Enum.IsDefined(typeof(Role), n))
-                return Enum.GetName((Role)n);
-
-            return null;
-        }
-
-        public static int? GetPriority(this Role role)
-        {
-            RoleInfoAttribute[] attributes = (RoleInfoAttribute[])
-            role.GetType().GetField(Enum.GetName(role)!)!
-            .GetCustomAttributes(typeof(RoleInfoAttribute), false);
-
-            return attributes.Length > 0 ? attributes[0].Priority : null;
-        }
+        return attributes.Length > 0 ? attributes[0].Priority : null;
     }
 }
--- src/Foundation.Common/Services/AuthorizationService.cs
+++ src/Foundation.Common/Services/AuthorizationService.cs
@@ -4,79 +4,78 @@ using Foundation.Common.Roles;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json;
 
-namespace Foundation.Common.Services
+namespace Foundation.Common.Services;
+
+public interface IAuthorizationService
 {
-    public interface IAuthorizationService
+    Task<TokenValidationResult> CheckAuthorizationAsync(string jwt);
+    List<Role> ExtractRoles(TokenValidationResult validationResult);
+}
+
+public class AuthorizationService : IAuthorizationService
+{
+    private readonly JwtSecurityTokenHandler TokenHandler;
+
+    public AuthorizationService(JwtSecurityTokenHandler tokenHandler)
     {
-        Task<TokenValidationResult> CheckAuthorizationAsync(string jwt);
-        List<Role> ExtractRoles(TokenValidationResult validationResult);
+        TokenHandler = tokenHandler;
     }
 
-    public class AuthorizationService : IAuthorizationService
+    /// <summary>
+    /// Checks if a token is valid.
+    /// </summary>
+    /// <param name="jwt">The JSON Web Token string.</param>
+    /// <returns>A TokenValidationResult object.</returns>
+    public async Task<TokenValidationResult> CheckAuthorizationAsync(string jwt)
     {
-        private readonly JwtSecurityTokenHandler TokenHandler;
-
-        public AuthorizationService(JwtSecurityTokenHandler tokenHandler)
+        TokenValidationResult result;
+        try
         {
-            TokenHandler = tokenHandler;
+            result = await TokenHandler.ValidateTokenAsync(jwt, Foundation.Common.Configurations.TokenConfiguration.ValidationParameters);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return new TokenValidationResult()
+            {
+                Exception = e,
+                IsValid = false
+            };
         }
 
-        /// <summary>
-        /// Checks if a token is valid.
-        /// </summary>
-        /// <param name="jwt">The JSON Web Token string.</param>
-        /// <returns>A TokenValidationResult object.</returns>
-        public async Task<TokenValidationResult> CheckAuthorizationAsync(string jwt)
-        {
-            TokenValidationResult result;
-            try
-            {
-                result = await TokenHandler.ValidateTokenAsync(jwt, Foundation.Common.Configurations.TokenConfiguration.ValidationParameters);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return new TokenValidationResult()
-                {
-                    Exception = e,
-                    IsValid = false
-                };
-            }
+        return result;
+    }
 
-            return result;
+    /// <summary>
+    /// Extracts an User object from a TokenValidationResult.
+    /// </summary>
+    /// <param name="validationResult">The TokenValidationResult</param>
+    /// <returns>An User object.</returns>
+    public User? ExtractUser(TokenValidationResult validationResult)
+    {
+        var json = validationResult.Claims.First(x => x.Key == "user").Value;
+
+        User? user;
+        try
+        {
+            user = JsonSerializer.Deserialize<User>((string)json);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null;
         }
 
-        /// <summary>
-        /// Extracts an User object from a TokenValidationResult.
-        /// </summary>
-        /// <param name="validationResult">The TokenValidationResult</param>
-        /// <returns>An User object.</returns>
-        public User? ExtractUser(TokenValidationResult validationResult)
-        {
-            var json = validationResult.Claims.First(x => x.Key == "user").Value;
+        return user;
+    }
 
-            User? user;
-            try
-            {
-                user = JsonSerializer.Deserialize<User>((string)json);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
-
-            return user;
-        }
-
-        /// <summary>
-        /// Extracts a list of Roles from a <c>TokenValidationResult</c>.
-        /// </summary>
-        /// <param name="validationResult">The TokenValidationResult</param>
-        /// <returns>A Role list.</returns>
-        public List<Role> ExtractRoles(TokenValidationResult validationResult)
-        {
-            return (ExtractUser(validationResult) ?? new()).Roles.ToList();
-        }
+    /// <summary>
+    /// Extracts a list of Roles from a <c>TokenValidationResult</c>.
+    /// </summary>
+    /// <param name="validationResult">The TokenValidationResult</param>
+    /// <returns>A Role list.</returns>
+    public List<Role> ExtractRoles(TokenValidationResult validationResult)
+    {
+        return (ExtractUser(validationResult) ?? new()).Roles.ToList();
     }
 }
