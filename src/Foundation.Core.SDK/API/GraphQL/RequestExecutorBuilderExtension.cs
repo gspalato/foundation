ecac1ca7f3a7829d5e36fbbbd1cf39fd29a262e3
Gabriel Spalato <3950465+gspalato@users.noreply.github.com>
2023-08-19 13:37:37 -0300

    Added comments to explicitly require the use of the fgen tool before compiling.

--- src/Foundation.Core.SDK/API/GraphQL/RequestExecutorBuilderExtension.cs
+++ src/Foundation.Core.SDK/API/GraphQL/RequestExecutorBuilderExtension.cs
@@ -7,6 +7,15 @@ namespace Foundation.Core.SDK.API.GraphQL;
 
 public static class RequestExecutorBuilderExtension
 {
+    /// <summary>
+    ///   Adds the generated query type to the GraphQL server.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    /// <remarks>
+    ///  This method requires the use of the <c>fgen</c> tool to generate the query type before compiling the code.
+    /// </remarks>
     public static IRequestExecutorBuilder AddGeneratedQueryType(this IRequestExecutorBuilder builder)
     {
         var queryType = Assembly
@@ -14,13 +23,23 @@ public static class RequestExecutorBuilderExtension
             .GetTypes()
             .FirstOrDefault(t => t.Name == "__foundationCodegen_QueryType" && t.IsClass && t.GetCustomAttributes<GeneratedAttribute>().Any());
             
-        if (queryType is null) throw new Exception("Could not find generated query type.");
+        if (queryType is null)
+            throw new Exception("Could not find generated query type. Did you run the fgen tool?");
 
         builder.AddQueryType(queryType);
 
         return builder;
     }
 
+    /// <summary>
+    ///   Adds the generated mutation type to the GraphQL server.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    /// <remarks>
+    ///  This method requires the use of the <c>fgen</c> tool to generate the query type before compiling the code.
+    /// </remarks>
     public static IRequestExecutorBuilder AddGeneratedMutationType(this IRequestExecutorBuilder builder)
     {
         var queryType = Assembly
@@ -28,7 +47,8 @@ public static class RequestExecutorBuilderExtension
             .GetTypes()
             .FirstOrDefault(t => t.Name == "__foundationCodegen_MutationType" && t.IsClass && t.GetCustomAttributes<GeneratedAttribute>().Any());
             
-        if (queryType is null) throw new Exception("Could not find generated query type.");
+        if (queryType is null)
+            throw new Exception("Could not find generated query type. Did you run the fgen tool?");
 
         builder.AddQueryType(queryType);
 
