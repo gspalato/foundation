190d0042bb9ca4be16f7ec248e812a29cf38a49b
gspalato <3950465+gspalato@users.noreply.github.com>
2023-10-13 23:45:11 -0300

    GetProfilePictureUploadUrl is now a query instead of a mutation.

--- services/identity/Types/Payloads/ProfilePictureUploadUrlPayload.cs
+++ services/identity/Types/Payloads/ProfilePictureUploadUrlPayload.cs
@@ -2,5 +2,5 @@ public class ProfilePictureUploadUrlPayload
 {
     public bool Successful { get; set; } = false;
     public string? Url { get; set; } = default!;
-    public string? Error { get; set; } = "Unallowed.";
+    public string? Error { get; set; } = default!;
 }
--- services/identity/Types/Query.cs
+++ services/identity/Types/Query.cs
@@ -10,7 +10,8 @@ namespace Foundation.Services.Identity.Types;
 // foundation generate query
 public class Query
 {
-    public async Task<CheckAuthenticationPayload> CheckAuthenticationAsync(string token, [Service] IAuthorizationService authService, [Service] IUserService userService)
+    public async Task<CheckAuthenticationPayload> CheckAuthenticationAsync(string token,
+    [Service] IAuthorizationService authService, [Service] IUserService userService)
     {
         var invalidTokenError = ErrorBuilder.New()
             .SetMessage("Invalid token. Try regenerating your token.")
@@ -23,7 +24,7 @@ public class Query
         var result = await authService.CheckAuthorizationAsync(token);
         if (!result.IsValid)
             throw new GraphQLException(invalidTokenError);
-            
+
         var id = result.Claims.FirstOrDefault(c => c.Key == "id").Value.ToString();
         if (id is null || id.Length is 0)
             throw new GraphQLException(invalidTokenError);
@@ -35,5 +36,40 @@ public class Query
             Successful = result.IsValid,
             User = user
         };
+    }
+
+    public async Task<ProfilePictureUploadUrlPayload> GetProfilePictureUploadUrlAsync(string token,
+    [Service] IAuthorizationService authorizationService, [Service] IUserService userService)
+    {
+        var result = await authorizationService.CheckAuthorizationAsync(token);
+        if (!result.IsValid)
+            return new ProfilePictureUploadUrlPayload
+            {
+                Successful = false,
+                Error = "Not authorized."
+            };
+
+        if (result.Claims.TryGetValue("id", out var userId))
+        {
+            var url = await userService.GetProfilePictureUploadUrlAsync((string)userId);
+            if (url is not null)
+                return new ProfilePictureUploadUrlPayload
+                {
+                    Successful = true,
+                    Url = url
+                };
+            else
+                return new ProfilePictureUploadUrlPayload
+                {
+                    Successful = false,
+                    Error = "Failed to get signed URL."
+                };
+        }
+        else
+            return new ProfilePictureUploadUrlPayload
+            {
+                Successful = false,
+                Error = "Invalid token."
+            };
     }
 }
